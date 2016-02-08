35ba5b1b6cdece8df39d84048c88e406be8fb2ce
Saulo Santiago <saulodasilvasantiago@gmail.com>
2016-02-08 03:22:55 -0200

    Add project structure

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+### [0.0.1](https://github.com/jcep/jcep/releases/tag/v0.0.1)
+
+- The first release
---
+++ test/unit/jcep.js
@@ -0,0 +1,18 @@
+import jcep from '../../src/jcep';
+
+describe('jcep', () => {
+  describe('Greet function', () => {
+    beforeEach(() => {
+      // spy(jcep, 'greet');
+      // jcep.greet();
+    });
+
+    it('should have been run once', () => {
+      // expect(jcep.greet).to.have.been.calledOnce;
+    });
+
+    it('should have always returned hello', () => {
+      // expect(jcep.greet).to.have.always.returned('hello');
+    });
+  });
+});
