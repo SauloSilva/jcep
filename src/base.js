9ba9038502e2c72c5adc3a6ba541b8ea8fc476f1
Saulo Santiago <saulodasilvasantiago@gmail.com>
2016-02-08 03:23:26 -0200

    Add Base class

---
+++ src/base.js
@@ -0,0 +1,135 @@
+class Base {
+  constructor(object = {}) {
+    let {
+      triggerEventName = false,
+      elements = false,
+      success: callbackSuccess,
+      error: callbackError
+    } = object;
+
+    let {
+      cepEl = 'cep',
+      streetEl = 'street',
+      numberEl = 'number',
+      neighborhoodEl = 'neighborhood',
+      cityEl = 'city',
+      stateEl = 'state'
+    } = elements;
+
+    this.cepEl = cepEl;
+    this.streetEl = streetEl;
+    this.numberEl = numberEl;
+    this.neighborhoodEl = neighborhoodEl;
+    this.cityEl = cityEl;
+    this.stateEl = stateEl;
+
+    this.triggerEventName = triggerEventName;
+    this.callbackSuccess = callbackSuccess || false;
+    this.callbackError = callbackError || false;
+  }
+
+  set cepEl(el) {
+    this._cepEl = this.searchEl({ el });
+  }
+
+  get cepEl() {
+    return this._cepEl;
+  }
+
+  set streetEl(el) {
+    this._streetEl = this.searchEl({ el });
+  }
+
+  get streetEl() {
+    return this._streetEl;
+  }
+
+  set neighborhoodEl(el) {
+    this._neighborhoodEl = this.searchEl({ el });
+  }
+
+  get neighborhoodEl() {
+    return this._neighborhoodEl;
+  }
+
+  set cityEl(el) {
+    this._cityEl = this.searchEl({ el });
+  }
+
+  get cityEl() {
+    return this._cityEl;
+  }
+
+  set stateEl(el) {
+    this._stateEl = this.searchEl({ el });
+  }
+
+  get stateEl() {
+    return this._stateEl;
+  }
+
+  set numberEl(el) {
+    this._numberEl = this.searchEl({ el });
+  }
+
+  get numberEl() {
+    return this._numberEl;
+  }
+
+  set triggerEventName(triggerEventName) {
+    this._triggerEventName = triggerEventName;
+  }
+
+  get triggerEventName() {
+    return this._triggerEventName;
+  }
+
+  set callbackSuccess(callbackSuccess) {
+    this._callbackSuccess = callbackSuccess;
+  }
+
+  get callbackSuccess() {
+    return this._callbackSuccess;
+  }
+
+  set callbackError(callbackError) {
+    this._callbackError = callbackError;
+  }
+
+  get callbackError() {
+    return this._callbackError;
+  }
+
+  set cep(cep) {
+    this._cep = cep.match(/[0-9]/g).join('');
+  }
+
+  get cep() {
+    return this._cep;
+  }
+
+  set jsonCallbackName(prefix) {
+    return this._jsonCallbackName = `jsonp_callback_${prefix}`;
+  }
+
+  get jsonCallbackName() {
+    return this._jsonCallbackName;
+  }
+
+  searchEl({ el }) {
+    let element;
+
+    if (el) {
+      element = document.querySelectorAll(el)[0];
+
+      if (!element) {
+        let elWithoutSpecialChar = el.match(/[a-zA-Z]/g).join('');
+        element = document.querySelectorAll(`[data-${elWithoutSpecialChar}]`)[0];
+      }
+    }
+
+    return element;
+  }
+}
+
+module.exports = Base;
---
+++ test/unit/base.js
@@ -0,0 +1,212 @@
+import Base from '../../src/base';
+
+describe('Base', () => {
+  let base;
+
+  describe('constructor function', () => {
+    let success = (() => {});
+    let error = (() => {});
+
+    describe('when triggerEventName is empty', () => {
+      beforeEach(() => {
+        base = new Base();
+      });
+
+      it('triggerEventName is false', () => {
+        expect(base.triggerEventName).to.equal(false);
+      });
+    });
+
+    describe('when triggerEventName exists', () => {
+      beforeEach(() => {
+        base = new Base({ triggerEventName: 'foo' });
+      });
+
+      it('returns name of the trigger event name', () => {
+        expect(base.triggerEventName).to.equal('foo');
+      });
+    });
+
+    describe('when elements is selector wrong', () => {
+      beforeEach(() => {
+        base = new Base({
+          elements: {
+            cepEl: '[name="test"]',
+            streetEl: '[name="test"]',
+            numberEl: '[name="test"]',
+            neighborhoodEl: '[name="test"]',
+            cityEl: '[name="test"]',
+            stateEl: '[name="test"]',
+          }
+        });
+      });
+
+      it('cepEl does not exists in the DOM', () => {
+        expect(base.cepEl).to.equal(undefined);
+      });
+
+      it('streetEl does not exists in the DOM', () => {
+        expect(base.streetEl).to.equal(undefined);
+      });
+
+      it('numberEl does not exists in the DOM', () => {
+        expect(base.numberEl).to.equal(undefined);
+      });
+
+      it('neighborhoodEl does not exists in the DOM', () => {
+        expect(base.neighborhoodEl).to.equal(undefined);
+      });
+
+      it('cityEl does not exists in the DOM', () => {
+        expect(base.cityEl).to.equal(undefined);
+      });
+
+      it('stateEl does not exists in the DOM', () => {
+        expect(base.stateEl).to.equal(undefined);
+      });
+    });
+
+    describe('when elements is present', () => {
+      beforeEach(() => {
+        base = new Base({
+          elements: {
+            cepEl: '[name="cep"]',
+            streetEl: '[name="street"]',
+            numberEl: '[name="number"]',
+            neighborhoodEl: '[name="neighborhood"]',
+            cityEl: '[name="city"]',
+            stateEl: '[name="state"]',
+          }
+        });
+      });
+
+      it('cepEl exists in the DOM', () => {
+        expect(base.cepEl).to.not.equal(undefined);
+      });
+
+      it('streetEl exists in the DOM', () => {
+        expect(base.streetEl).to.not.equal(undefined);
+      });
+
+      it('numberEl exists in the DOM', () => {
+        expect(base.numberEl).to.not.equal(undefined);
+      });
+
+      it('neighborhoodEl exists in the DOM', () => {
+        expect(base.neighborhoodEl).to.not.equal(undefined);
+      });
+
+      it('cityEl exists in the DOM', () => {
+        expect(base.cityEl).to.not.equal(undefined);
+      });
+
+      it('stateEl exists in the DOM', () => {
+        expect(base.stateEl).to.not.equal(undefined);
+      });
+    });
+
+    describe('when elements is empty', () => {
+      beforeEach(() => {
+        base = new Base();
+      });
+
+      it('cepEl exists in the DOM', () => {
+        expect(base.cepEl).to.not.equal(undefined);
+      });
+
+      it('streetEl exists in the DOM', () => {
+        expect(base.streetEl).to.not.equal(undefined);
+      });
+
+      it('numberEl exists in the DOM', () => {
+        expect(base.numberEl).to.not.equal(undefined);
+      });
+
+      it('neighborhoodEl exists in the DOM', () => {
+        expect(base.neighborhoodEl).to.not.equal(undefined);
+      });
+
+      it('cityEl exists in the DOM', () => {
+        expect(base.cityEl).to.not.equal(undefined);
+      });
+
+      it('stateEl exists in the DOM', () => {
+        expect(base.stateEl).to.not.equal(undefined);
+      });
+    });
+
+    describe('when success callback is empty', () => {
+      beforeEach(() => {
+        base = new Base();
+      });
+
+      it('callbackSuccess is false', () => {
+        expect(base.callbackSuccess).to.equal(false);
+      });
+    });
+
+    describe('when success callback exists', () => {
+      beforeEach(() => {
+        base = new Base({ success: success });
+      });
+
+      it('returns callback of the callbackSuccess', () => {
+        expect(base.callbackSuccess).to.equal(success);
+      });
+    });
+
+    describe('when error callback is empty', () => {
+      beforeEach(() => {
+        base = new Base();
+      });
+
+      it('callbackError is false', () => {
+        expect(base.callbackError).to.equal(false);
+      });
+    });
+
+    describe('when error callback exists', () => {
+      beforeEach(() => {
+        base = new Base({ error: error });
+      });
+
+      it('returns callback of the callbackError', () => {
+        expect(base.callbackError).to.equal(error);
+      });
+    });
+  });
+
+  describe('searchEl function', () => {
+    beforeEach(() => {
+      base = new Base();
+    });
+
+    describe('when selector el is empty', () => {
+      it('selector el is equal to undefined', () => {
+        let el = '';
+        expect(base.searchEl({ el })).to.equal(undefined);
+      });
+    });
+
+    describe('when selector el does not exists', () => {
+      it('selector el is equal to undefined', () => {
+        let el = '.wrong-el';
+        expect(base.searchEl({ el })).to.equal(undefined);
+      });
+    });
+
+    describe('when selector el is totally correct', () => {
+      it('selector el is not equal to undefined', () => {
+        let el = '[name="cep"]';
+        expect(base.searchEl({ el })).to.not.equal(undefined);
+      });
+    });
+
+    describe('when selector el is wrong but found by data attributes', () => {
+      it('selector el is not equal to undefined', () => {
+        let el = 'cep';
+        expect(base.searchEl({ el })).to.not.equal(undefined);
+      });
+    });
+  });
+});
